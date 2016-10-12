c8c2382fc97bc9b0aa974d068595dcbc12093c3d
AudioLoop <brendanmusic1738@gmail.com>
2016-10-11 21:54:13 -0400

    test
    test

---
+++ js/my-app.js
@@ -0,0 +1,13 @@
+// Initialize app and store it to myApp variable for futher access to its methods
+var myApp = new Framework7();
+ 
+// We need to use custom DOM library, let's save it to $$ variable:
+var $$ = Dom7;
+ 
+// Add view
+var mainView = myApp.addView('.view-main', {
+  // Because we want to use dynamic navbar, we need to enable it for this view:
+  dynamicNavbar: true
+});
+ 
+// Now we need to run the code that will be executed only for About page.
